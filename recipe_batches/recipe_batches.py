b773136627d3af65252ea3a871ae75939ed5c41c
Dpgofast <43558343+Dpgofast@users.noreply.github.com>
2019-12-12 15:54:36 -0600

    Finished recipe_batches ✅

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,27 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    max_batch = 0
+    limit = None
+
+    for key, value in recipe.items():
+        if key not in list(ingredients.keys()):
+            return 0
+        if key in ingredients:
+            take = ingredients[key] // value
+        if limit is None:
+            max_batch = take
+            limit = max_batch
+        elif take < limit:
+            limit = take
+    return max_batch
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
